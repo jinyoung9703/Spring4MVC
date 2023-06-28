270c33cee1ea9cdf26bcaf99e3d20cade317b4f7
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-06-28 13:53:14 +0900

    MemberController 작성

---
+++ src/main/java/com/hello/spring4/controller/MemberController.java
@@ -0,0 +1,42 @@
+package com.hello.spring4.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MemberController {
+
+
+    private Logger logger = LogManager.getLogger(MemberController.class);
+
+    @RequestMapping("/member/join")
+    public String join(Model m){
+
+        // 로거 출력
+        logger.info("member/join 호출!");
+
+        return "member/join.tiles";
+    }
+    @RequestMapping("/member/login")
+    public String login(Model m){
+
+        // 로거 출력
+        logger.info("member/login 호출!");
+
+        return "member/login.tiles";
+    }
+    @RequestMapping("/member/myinfo")
+    public String myinfo(Model m){
+
+        // 로거 출력
+        logger.info("member/myinfo 호출!");
+
+        return "member/myinfo.tiles";
+    }
+
+
+}
