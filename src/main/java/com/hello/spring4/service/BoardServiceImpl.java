cb91e8a1100e71d5782d42c64fdde4df41df1299
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-06-30 16:53:31 +0900

    BoardService : readBoard 구현

---
+++ src/main/java/com/hello/spring4/service/BoardServiceImpl.java
@@ -0,0 +1,27 @@
+package com.hello.spring4.service;
+
+import com.hello.spring4.dao.BoardDAO;
+import com.hello.spring4.model.Board;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Service("bsrv")
+public class BoardServiceImpl implements BoardService{
+
+    @Autowired BoardDAO bdao;
+
+    @Override
+    public List<Board> readBoard(int cpg) {
+        int snum = (cpg - 1) * 15;
+
+        return bdao.selectBoard(snum);
+
+
+    }
+
+}
