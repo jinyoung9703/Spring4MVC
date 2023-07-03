ebc3fbf67c8ff02a3965d017216eb7b14ce566ce
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-03 14:54:05 +0900

    BoardController : writeok 구현

--- src/main/java/com/hello/spring4/controller/BoardController.java
+++ src/main/java/com/hello/spring4/controller/BoardController.java
@@ -6,48 +6,53 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
-    @Autowired
-    BoardService bsrv;
+    @Autowired BoardService bsrv;
 
     private Logger logger = LogManager.getLogger(BoardController.class);
 
-   @GetMapping("/list")
-   public String list(Model m, int cpg) {
-       logger.info("board/list 호출!");
+    @GetMapping("/list")
+    public String list(Model m, int cpg) {
+        logger.info("board/list 호출!!");
 
-       m.addAttribute("boards", bsrv.readBoard(cpg));
-//       m.addAttribute("psnum", );   // 페이지네이션 시작번호
-//       m.addAttribute("allpg", );   // 총페이지수
+        m.addAttribute("boards", bsrv.readBoard(cpg));
 
-       return "board/list.tiles";
-   }  @GetMapping("/write")
-   public String write(){
-       return "board/write.tiles";
-   }
+        return "board/list.tiles";
+    }
 
+    @GetMapping("/write")
+    public String write() {
 
-   public String view(){
-       return "board/view.tiles";
-   }
+        return "board/write.tiles";
+    }
+
+    @PostMapping("/write")
+    public String writeok(Board bd){
+        String returnPage ="redirect:/board/fail";
+
+        if (bsrv.saveBoard(bd))
+        returnPage = "redirect:/board/list?cpg=1";
+
+        return returnPage;
+    }
+
     @GetMapping("/view")
-    public String view(Model m, String bno){
-        logger.info("board/view 호풀!!");
+    public String view(Model m, String bno) {
+        logger.info("board/view 호출!!");
         Board board = bsrv.readOneBoard(bno);
 
-        m.addAttribute("board",board);
+        m.addAttribute("board", board);
 
         return "board/view.tiles";
     }
-
-
-
 
 }
