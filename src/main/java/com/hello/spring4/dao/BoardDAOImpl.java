afb8b1f1145c29c19e65918ed6d93c99795f2dbf
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-03 14:51:52 +0900

    boardDAO : 죄회수 증가, insertBoard 구현

--- src/main/java/com/hello/spring4/dao/BoardDAOImpl.java
+++ src/main/java/com/hello/spring4/dao/BoardDAOImpl.java
@@ -16,6 +16,8 @@ public class BoardDAOImpl implements BoardDAO{
 
     @Value("#{sql['selectBoard']}") private String selectSQL;
     @Value("#{sql['selectOneBoard']}") private String selectOneSQL;
+    @Value("#{sql['viewCountBoard']}") private String viewCountSQL;
+    @Value("#{sql['insertBoard']}") private String insertSQL;
 
     @Autowired JdbcTemplate jdbcTemplate;
     @Override
@@ -28,33 +30,46 @@ public class BoardDAOImpl implements BoardDAO{
     @Override
     public Board selectOneBoard(String bno) {
         Object[] params = new Object[] {bno};
-        RowMapper<Board> mapper = new SelectMapper();
+        RowMapper<Board> mapper = new SelectOneMapper();
+
+        //조회수 증가
+        jdbcTemplate.update(viewCountSQL,params);
 
         return jdbcTemplate.queryForObject(
                 selectOneSQL, params, mapper);
     }
 
+    @Override
+    public int insertBoard(Board bd) {
+        Object[] params = new Object[]{
+                bd.getTitle(), bd.getUserid(), bd.getContents()
+        };
+
+        return jdbcTemplate.update(insertSQL,params);
+    }
+
     private class SelectMapper implements RowMapper<Board> {
         @Override
         public Board mapRow(ResultSet rs, int num) throws SQLException {
-            Board bd = new Board(rs.getString(1),rs.getString(2),
-                                rs.getString(3),rs.getString(4),
-                                rs.getString(5),null);
+            Board bd = new Board(rs.getString(1),
+                    rs.getString(2),rs.getString(3),
+                    rs.getString(4),rs.getString(5),
+                    null);
+
             return bd;
         }
-    }// selectmapper
-
+    } // selectmapper
     private class SelectOneMapper implements RowMapper<Board> {
         @Override
         public Board mapRow(ResultSet rs, int num) throws SQLException {
             Board bd = new Board(rs.getString(1),
-                    rs.getString(2),
-                    rs.getString(3),rs.getString(4),
-                    rs.getString(5),rs.getString(6));
+                    rs.getString(2),rs.getString(3),
+                    rs.getString(4),rs.getString(5),
+                    rs.getString(6));
 
             return bd;
         }
-    }
+    } // selectonemapper
 
+} // class
 
-}
