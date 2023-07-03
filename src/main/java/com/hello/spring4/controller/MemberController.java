9ad214860b61c094e8df544ead56515520008329
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-03 17:01:27 +0900

    pjp에 전달할 세션객체 선언

--- src/main/java/com/hello/spring4/controller/MemberController.java
+++ src/main/java/com/hello/spring4/controller/MemberController.java
@@ -23,7 +23,7 @@ public class MemberController {
     private MemberService msrv;
 
     @RequestMapping(value = "/member/join", method = RequestMethod.GET)
-    public String join(Model m){
+    public String join(Model m, HttpSession sess){
 
         // 로거 출력
         logger.info("member/join 호출!");
