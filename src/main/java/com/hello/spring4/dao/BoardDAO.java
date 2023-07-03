79e1a55bda09d55577e8f13d1ed67d75549b5db5
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-03 11:11:05 +0900

    boardDAO : selectOneBoard 구현

--- src/main/java/com/hello/spring4/dao/BoardDAO.java
+++ src/main/java/com/hello/spring4/dao/BoardDAO.java
@@ -5,7 +5,6 @@ import com.hello.spring4.model.Board;
 import java.util.List;
 
 public interface BoardDAO {
-    default List<Board> selectBoard(int snum) {
-        return null;
-    }
+    List<Board> selectBoard(int snum);
+    Board selectOneBoard(String bno);
 }
--- src/main/java/com/hello/spring4/dao/BoardDAOImpl.java
+++ src/main/java/com/hello/spring4/dao/BoardDAOImpl.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class BoardDAOImpl implements BoardDAO{
 
     @Value("#{sql['selectBoard']}") private String selectSQL;
+    @Value("#{sql['selectOneBoard']}") private String selectOneSQL;
 
     @Autowired JdbcTemplate jdbcTemplate;
     @Override
@@ -23,6 +24,14 @@ public class BoardDAOImpl implements BoardDAO{
         RowMapper<Board> mapper = new SelectMapper();
 
         return jdbcTemplate.query(selectSQL,params,mapper);
+    }
+    @Override
+    public Board selectOneBoard(String bno) {
+        Object[] params = new Object[] {bno};
+        RowMapper<Board> mapper = new SelectMapper();
+
+        return jdbcTemplate.queryForObject(
+                selectOneSQL, params, mapper);
     }
 
     private class SelectMapper implements RowMapper<Board> {
@@ -33,5 +42,19 @@ public class BoardDAOImpl implements BoardDAO{
                                 rs.getString(5),null);
             return bd;
         }
+    }// selectmapper
+
+    private class SelectOneMapper implements RowMapper<Board> {
+        @Override
+        public Board mapRow(ResultSet rs, int num) throws SQLException {
+            Board bd = new Board(rs.getString(1),
+                    rs.getString(2),
+                    rs.getString(3),rs.getString(4),
+                    rs.getString(5),rs.getString(6));
+
+            return bd;
+        }
     }
+
+
 }
